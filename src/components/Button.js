2020ec91de3d20f6ed18ef938f78809d5e75a336
soran011 <haruti.soran@gmail.com>
2020-08-06 15:50:30 -0400

    created React app, and two components

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class Buttons extends Component {
+    render() {
+        return (
+            <div>
+                <button className="calc-button">1</button>
+            </div>
+        )
+    }
+}
+
+export default Buttons;
